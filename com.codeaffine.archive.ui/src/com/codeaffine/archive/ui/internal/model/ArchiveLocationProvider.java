d6f4d26b635346da61b6fa675473cb170ee80416
Ruediger Herrmann <ruediger.herrmann@gmx.de>
2012-01-30 22:32:56 +0100

    Removed files that were accidentially pulled into gh-pages

--- com.codeaffine.archive.ui/src/com/codeaffine/archive/ui/internal/model/ArchiveLocationProvider.java
+++
@@ -1,9 +0,0 @@
-package com.codeaffine.archive.ui.internal.model;
-
-import org.eclipse.core.runtime.IPath;
-
-
-interface ArchiveLocationProvider {
-  String getArchiveName();
-  IPath getLocation();
-}
