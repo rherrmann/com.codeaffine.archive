94518130a6c4f84a264bc4302dfd830ddddbb941
Ruediger Herrmann <ruediger.herrmann@gmx.de>
2012-02-21 23:34:52 +0100

    Added further integration tests

--- com.codeaffine.archive.ui.itest/src/com/codeaffine/archive/ui/internal/editor/EditorTest.java
+++ com.codeaffine.archive.ui.itest/src/com/codeaffine/archive/ui/internal/editor/EditorTest.java
@@ -9,6 +9,7 @@ import org.eclipse.swt.widgets.Tree;
 import org.eclipse.swtbot.eclipse.finder.SWTWorkbenchBot;
 import org.eclipse.swtbot.eclipse.finder.widgets.SWTBotView;
 import org.eclipse.swtbot.swt.finder.junit.SWTBotJunit4ClassRunner;
+import org.eclipse.swtbot.swt.finder.keyboard.Keystrokes;
 import org.eclipse.swtbot.swt.finder.matchers.AllOf;
 import org.eclipse.swtbot.swt.finder.matchers.WidgetMatcherFactory;
 import org.eclipse.swtbot.swt.finder.widgets.SWTBotTree;
@@ -25,26 +26,56 @@ import com.codeaffine.archive.testfixture.TestProject;
 
 @RunWith(SWTBotJunit4ClassRunner.class)
 public class EditorTest {
+  private static final String ARCHIVE_ZIP = "archive.zip";
+  private static final String FILE_TXT = "file.txt";
 
   private TestProject testProject;
   private SWTWorkbenchBot bot;
 
   @Test
-  public void testOpenArchiveEntryInEditor() throws Exception {
-    provisionArchiveFile();
-    SWTBotView view = bot.viewById( ProjectExplorer.VIEW_ID );
-    view.show();
-    SWTBotTree tree = new SWTBotTree( bot.widget( allTreesMatcher(), view.getWidget() ) );
-    SWTBotTreeItem treeItem = tree.expandNode( testProject.getName(), "archive.zip" ).getNode( "file.txt" );
-    treeItem.doubleClick();
+  public void testOpenArchiveEntryByDoubleClick() {
+    SWTBotTree tree = getProjectExplorerTree();
+    SWTBotTreeItem fileItem = selectArchiveEntry( tree );
+    fileItem.doubleClick();
 
-    assertEquals( "file.txt", bot.activeEditor().getTitle() );
+    assertEquals( FILE_TXT, bot.activeEditor().getTitle() );
+  }
+
+// TODO [rh] Disabled until bug 372170 (pressShortcut( Keystrokes.CR ) fails with "Invalid key code")
+//      is resolved
+//  @Test
+//  public void testOpenArchiveEntryByReturnKey() {
+//    SWTBotTree tree = getProjectExplorerTree();
+//    SWTBotTreeItem fileItem = selectArchiveEntry( tree );
+//    fileItem.pressShortcut( Keystrokes.CR );
+//
+//    assertEquals( FILE_TXT, bot.activeEditor().getTitle() );
+//  }
+
+  @Test
+  public void testOpenArchiveEntryByContextMenu() {
+    SWTBotTree tree = getProjectExplorerTree();
+    SWTBotTreeItem fileItem = selectArchiveEntry( tree );
+    fileItem.contextMenu( "Open" ).click();
+
+    assertEquals( FILE_TXT, bot.activeEditor().getTitle() );
+  }
+
+  @Test
+  public void testOpenSameEntryReusesEditor() {
+    SWTBotTree tree = getProjectExplorerTree();
+    SWTBotTreeItem fileItem = selectArchiveEntry( tree );
+    fileItem.doubleClick();
+    fileItem.doubleClick();
+
+    assertEquals( 1, bot.editors().size() );
   }
 
   @Before
-  public void setUp() {
+  public void setUp() throws Exception {
     testProject = new TestProject();
     bot = new SWTWorkbenchBot();
+    provisionArchiveFile();
   }
 
   @After
@@ -56,10 +87,21 @@ public class EditorTest {
   private void provisionArchiveFile() throws Exception {
     InputStream inputStream = getClass().getResourceAsStream( "archive.zip" );
     try {
-      TestProject.createFile( testProject.getProject(), "archive.zip", inputStream );
+      TestProject.createFile( testProject.getProject(), ARCHIVE_ZIP, inputStream );
     } finally {
       inputStream.close();
     }
+  }
+
+  private SWTBotTree getProjectExplorerTree() {
+    bot.viewById( ProjectExplorer.VIEW_ID ).show();
+    SWTBotView view = bot.viewById( ProjectExplorer.VIEW_ID );
+    return new SWTBotTree( bot.widget( allTreesMatcher(), view.getWidget() ) );
+  }
+
+  private SWTBotTreeItem selectArchiveEntry( SWTBotTree tree ) {
+    SWTBotTreeItem archiveItem = tree.expandNode( testProject.getName(), ARCHIVE_ZIP );
+    return archiveItem.getNode( FILE_TXT );
   }
 
   @SuppressWarnings("unchecked")
